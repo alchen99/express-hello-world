8ddf46f3b2e3a8755a3976f2dca3cbc18aa6fbb4
Alice Chen <alchen99@users.noreply.github.com>
2020-09-18 14:03:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello from Armory!')
+  res.send('Hello everyone from Armory!')
 })
 
 app.listen(3000, function () {
