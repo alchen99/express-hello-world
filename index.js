a19a6097e1ef8ccd8f5da7be3b94f2bff86ea2b4
Alice Chen <alice.chen@armory.io>
2020-10-27 12:47:45 -0700

    Update app

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello everyone from Pluto!')
-})
+app.use(express.static(__dirname + '/www'));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000...')
